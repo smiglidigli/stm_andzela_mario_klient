eadf970e5ca63b180bda08bad67cd89a0890bab9
angie <angwittb@gmail.com>
2018-11-03 16:02:32 +0100

    first working version

--- app/src/main/java/stm/msu/mapa/stm_andzela_mario_klient/MainActivity.java
+++ app/src/main/java/stm/msu/mapa/stm_andzela_mario_klient/MainActivity.java
@@ -11,11 +11,15 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.Marshal;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapPrimitive;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 import org.ksoap2.serialization.PropertyInfo;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlSerializer;
 
 import android.util.AttributeSet;
 import android.util.Base64;
@@ -27,20 +31,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import java.io.FileNotFoundException;
-import java.math.BigDecimal;
+import java.io.IOException;
 
-public class MainActivity extends Activity {
-    private String URL = "http://127.0.0.1:8080/MapaWebService/MapaWebService?WSDL";
-    private String NAMESPACE = "http://stm_andzela_mario_serwer/";
-    private String METHOD_NAME = "get_small_map";
-    private String SOAP_ACTION =  "http://stm_andzela_mario/get_small_map";
-    private String MAP_METHOD_NAME = "map";
-    private String MAP_SOAP_ACTION =  "http://stm_andzela_mario/map";
+public class MainActivity extends Activity implements Marshal {
+    private String URL = "http://10.0.2.2:8080/ImageEncoder/WebEncoder?WSDL";
+    private String NAMESPACE = "http://encoder.angelika.org/";
+    private String METHOD_GET_RESIZED_IMAGE = "crop";
+    private String SOAP_ACTION_GET_RESIZED_IMAGE =  "http://encoder.angelika.org/crop/";
+    private String METHOD_GET_FULL_IMAGE = "getBinaryImage";
+    private String SOAP_ACTION_GET_FULL_IMAGE =  "http://encoder.angelika.org/getBinaryImage/";
     ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button go_button = findViewById(R.id.go_button);
         go_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
@@ -80,6 +85,23 @@ public class MainActivity extends Activity {
 
 
     }
+
+    @Override
+    public Object readInstance(XmlPullParser parser, String namespace, String name, PropertyInfo expected) throws IOException, XmlPullParserException {
+        return Double.parseDouble(parser.nextText());
+    }
+
+    @Override
+    public void writeInstance(XmlSerializer writer, Object instance) throws IOException {
+        writer.text(instance.toString());
+    }
+
+    @Override
+    public void register(SoapSerializationEnvelope envelope) {
+        envelope.addMapping(envelope.xsd, "double", Double.class, this);
+
+    }
+
     public class CallGetMapWebService extends AsyncTask<Double, Void, String> {
         @Override
         protected void onPostExecute(String s) {
@@ -90,21 +112,21 @@ public class MainActivity extends Activity {
 
         @Override
         protected String doInBackground(Double... params) {
-            SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+            SoapObject request = new SoapObject(NAMESPACE, METHOD_GET_RESIZED_IMAGE);
             PropertyInfo propertyInfo_lat1 = new PropertyInfo();
-            propertyInfo_lat1.setName("lat1");
+            propertyInfo_lat1.setName("x1");
             propertyInfo_lat1.setValue(params[0]);
             propertyInfo_lat1.setType(double.class);
             PropertyInfo propertyInfo_lat2 = new PropertyInfo();
-            propertyInfo_lat2.setName("lat2");
+            propertyInfo_lat2.setName("x2");
             propertyInfo_lat2.setValue(params[1]);
             propertyInfo_lat2.setType(double.class);
             PropertyInfo propertyInfo_lon1 = new PropertyInfo();
-            propertyInfo_lon1.setName("lat2");
+            propertyInfo_lon1.setName("y1");
             propertyInfo_lon1.setValue(params[1]);
             propertyInfo_lon1.setType(double.class);
             PropertyInfo propertyInfo_lon2 = new PropertyInfo();
-            propertyInfo_lon2.setName("lat2");
+            propertyInfo_lon2.setName("y2");
             propertyInfo_lon2.setValue(params[1]);
             propertyInfo_lon2.setType(double.class);
 
@@ -116,10 +138,12 @@ public class MainActivity extends Activity {
             SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
             envelope.dotNet = false;
             envelope.setOutputSoapObject(request);
+            register(envelope);
+
             HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
 
             try {
-                androidHttpTransport.call(SOAP_ACTION, envelope);
+                androidHttpTransport.call(SOAP_ACTION_GET_RESIZED_IMAGE, envelope);
                 SoapPrimitive  resultsRequestSOAP = (SoapPrimitive) envelope.getResponse();
                 return resultsRequestSOAP.toString();
             } catch (Exception e) {
@@ -140,16 +164,18 @@ public class MainActivity extends Activity {
 
         @Override
         protected String doInBackground(String... params) {
-            SoapObject request = new SoapObject(NAMESPACE, MAP_METHOD_NAME);
+            SoapObject request = new SoapObject(NAMESPACE, METHOD_GET_FULL_IMAGE);
             SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
             envelope.dotNet = false;
             envelope.setOutputSoapObject(request);
+            register(envelope);
             HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
 
             try {
-                androidHttpTransport.call(MAP_SOAP_ACTION, envelope);
+                androidHttpTransport.call(SOAP_ACTION_GET_FULL_IMAGE, envelope);
                 SoapPrimitive  resultsRequestSOAP = (SoapPrimitive) envelope.getResponse();
-                return resultsRequestSOAP.toString();
+                String x = resultsRequestSOAP.toString();
+                return x;
             } catch (Exception e) {
                 Log.e("Activity",e.toString());
             }
